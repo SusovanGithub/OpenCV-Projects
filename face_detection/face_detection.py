680c8426e62a1ef7b06aeacf3b0c5c8c6b09a7a8
Susovan Das <susovandas1612@gmail.com>
2021-05-07 03:34:30 +0530

    Face Detection Added

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -1,17 +1,18 @@
 import cv2
 import time
 
+face_cascade = cv2.CascadeClassifier('face_detect_cascade.xml')
+
 cam = cv2.VideoCapture(0)           # Creating the Webcam Instance
 
-# Setting the Resolution
-cam.set(3,640)                      # Width
-cam.set(4,480)                      # Height
-
-pretime = time.time()               # Setting time
+pretime = time.time()               # Setting time  
 
 while True:
     isTrue, frame = cam.read()      # Reading the Frames
     
+    # Mirror the frame output
+    frame = cv2.flip(frame,1)
+
     # Adding the FPS in video
     newtime = time.time()               # Taking the Current time
     fps = int(1/(newtime-pretime))      # Calculating the FPS
@@ -24,12 +25,18 @@ while True:
                 color=(255,0,0),
                 thickness=2)
     
+    # Detecting the face
+    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+    # Creating a box outline for the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Display the Frame
     cv2.imshow('Output',frame)
     
     # Creating the Exit Pole
     if cv2.waitKey(1) & 0xFF == 27:
         break
-    
+
 cam.release()                       # Releasing the instance
 cv2.destroyAllWindows()                 # Destroing the windows
--- mirror_webcam.py
+++ mirror_webcam.py
@@ -3,8 +3,8 @@ import time
 
 cam = cv2.VideoCapture(0)           # Creating the Webcam Instance
 
-address = 'http://192.168.0.101:4747/video'
-cam.open(address)
+# address = 'http://192.168.0.101:4747/video'
+# cam.open(address)
 
 pretime = time.time()                   # Setting time
 
@@ -12,17 +12,17 @@ pretime = time.time()                   # Setting time
 while True:
     isTrue, frame = cam.read()      # Reading the Frames
 
-    # frame = cv2.resize(frame,(640,480))
 
-    # Mirror the image output
+    # Mirror the frame output
     frame = cv2.flip(frame,1)
 
     # Converting into gray space
     # frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     
-    # Display the FPS in video
+    # Adding the FPS in video
     newtime = time.time()               # Taking the Current time
-    fps = int((newtime-pretime)*1000)   # Calculating the FPS
+    fps = int(1/(newtime-pretime))      # Calculating the FPS
+    pretime = newtime                   # Set the previous time
     cv2.putText(frame,                  # text adding to the frame 
                 text=str(fps) + 'fps',
                 org=(10,30),
@@ -30,10 +30,11 @@ while True:
                 fontScale=1,
                 color=(255,0,0),
                 thickness=2)
-    pretime = newtime                   # Set the previous time
-
-    # Creating the Exit Pole
+    
+    # Display the Frame
     cv2.imshow('Output',frame)
+    
+    # Creating the Exit Pole
     if cv2.waitKey(1) & 0xFF == 27:
         break
 

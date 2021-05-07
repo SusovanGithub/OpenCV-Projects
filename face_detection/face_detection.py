c64990e66b98fd6ebea1e3811144da792717b826
Susovan Das <susovandas1612@gmail.com>
2021-05-08 00:35:46 +0530

    Some change

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -1,12 +1,26 @@
 import cv2
 import time
 
+# A empty function
+def empty(a):
+    pass
+
+# Loading the classifier
 face_cascade = cv2.CascadeClassifier('face_detect_cascade.xml')
+
+# Creating a Window
+windowName = "Output"
+cv2.namedWindow(windowName)
+
+# # Creating the trackpad
+# cv2.createTrackbar('Scale Factor',windowName,1.1,9,empty)
+# cv2.createTrackbar('Min Neighbors',windowName,1,10,empty)
 
 cam = cv2.VideoCapture(0)           # Creating the Webcam Instance
 
 pretime = time.time()               # Setting time  
 
+# Video Starts
 while True:
     isTrue, frame = cam.read()      # Reading the Frames
     
@@ -25,14 +39,23 @@ while True:
                 color=(255,0,0),
                 thickness=2)
     
+    frameGray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    # # Getting the track bar values
+    # scale = cv2.getTrackbarPos('Scale Factor',windowName) + 1     # Scale Factor
+    # minNgh = cv2.getTrackbarPos('Min Neighbors',windowName)       # Min Neighbors
+
+    scale = 1.1
+    minNgh = 4
+
     # Detecting the face
-    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+    faces = face_cascade.detectMultiScale(frameGray,scale,minNgh)
     # Creating a box outline for the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the Frame
-    cv2.imshow('Output',frame)
+    cv2.imshow(windowName,frame)
     
     # Creating the Exit Pole
     if cv2.waitKey(1) & 0xFF == 27:

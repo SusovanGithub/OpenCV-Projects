15f29eb77cbf08b1b20f6f252bc2959b65d8a308
Susovan Das <susovandas1612@gmail.com>
2021-05-11 21:44:22 +0530

    image_warp_perspective.py added

--- image_warp_perspective.py
+++ image_warp_perspective.py
@@ -12,17 +12,19 @@ def onClick(event,x,y,flag,img):
     global circle,counter                               # ! Accessing the Global Variables
     if event == cv2.EVENT_LBUTTONDBLCLK:
         pts1[counter] = x,y
-        cv2.circle(img,(x,y),5,(0,0,255),-1)
         counter = counter + 1
+        cv2.circle(img,(x,y),4,(0,0,255),-1)
+        cv2.putText(img,str(counter),(x-5,y-10),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,.5,(0,0,255))
 
 # * Main Function
 def main():
     global pts1, counter, output_width, output_height   # ! Accessing the Global Variables
     
     # * Getting the Image Location
-    path = 'Assets/book3.jpeg' #input('Enter the Image location = ')
+    path = input('Enter the Image location = ')
     img = cv2.imread(path)
     imgC = img.copy()
+    
     # * Creating the Window
     cv2.namedWindow(windowName)
 
@@ -30,13 +32,13 @@ def main():
     cv2.setMouseCallback(windowName,onClick,img)
 
     while True:
-        # * Condition when 
+        # * Condition that click corners are clicked or not
         if counter == 4:
             pts1 = np.float32(pts1)
             pts2 = np.float32([[0,0],[output_width,0],[0,output_height],[output_width,output_height]])
             matix = cv2.getPerspectiveTransform(pts1,pts2)
             imgOut = cv2.warpPerspective(imgC,matix,(output_width,output_height))
-            cv2.imshow('Result',imgOut)
+            cv2.imshow('Result',imgOut)         # Display the resultent Output
 
         # * Display the Image
         cv2.imshow(windowName,img)
